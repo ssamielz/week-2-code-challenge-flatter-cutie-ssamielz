e08c16748a5b507bd9a3feeeae4dfddbbca2c83a
Sam <ssamsanya3@gmail.com>
2025-03-24 10:28:45 +0300

    names array

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 // Your code here
+function getCharacter() {
+
+fetch('http://localhost:3000/characters')
+    .then(response => response.json())
+    .then(animals => animals.forEach(animal => displayCharacters(animal)))
+}
+
+function displayCharacters(animal){
+    let characterBar = document.getElementById("character-bar")
+    let animalName = document.createElement("span")
+    animalName.innerHTML = `${animal.name}`
+    characterBar.appendChild(animalName)
+}
+
+getCharacter()
