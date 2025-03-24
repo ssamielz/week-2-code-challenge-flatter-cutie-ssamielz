9737818100162dce03314546d56d63a16f9e1c16
Sam <ssamsanya3@gmail.com>
2025-03-24 10:47:41 +0300

    linking image to click events

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
-// Your code here
-function getCharacter() {
 
+function getCharacter() {
+//fetching data from json server
 fetch('http://localhost:3000/characters')
     .then(response => response.json())
     .then(animals => animals.forEach(animal => displayCharacters(animal)))
 }
 
-function displayCharacters(animal){
+//display characters
+function displayCharacters(animal) {
+
     let characterBar = document.getElementById("character-bar")
     let animalName = document.createElement("span")
     animalName.innerHTML = `${animal.name}`
     characterBar.appendChild(animalName)
+    animalName.addEventListener('click',()=>{
+        console.log(`clicked ${animal.name}`)
+        document.getElementById("image").src = animal.image
+    });
+
+    
 }
 
 getCharacter()
+
