d76cf77e11144926c386ff60d39797873b98a258
Sam <ssamsanya3@gmail.com>
2025-03-29 11:50:21 +0300

    adding and reset votes

--- src/index.js
+++ src/index.js
@@ -22,13 +22,56 @@ function displayCharacters(animal) {
     //displaying character name and votes
         document.getElementById("name").innerHTML = animal.name
         document.getElementById("vote-count").innerHTML = `${animal.votes}`      
+        //add votes
         let votesForm = document.getElementById("votes-form")
-        votesForm.addEventListener("submit",()=>{
+        votesForm.addEventListener("submit",(event)=>{
             event.preventDefault();
-            let enteredVotes=parseInt(votesForm.querySelector("#votes").value)
-            document.getElementById("vote-count").innerHTML = enteredVotes + parseInt(document.getElementById("vote-count").innerHTML)     
+            let enteredVotes = parseInt(votesForm.querySelector("#votes").value,10)
+                console.log(enteredVotes);
+                console.log(animal.votes);
+            let newVotes = animal.votes + enteredVotes
 
+            let finder = animal.id
+
+            fetch(`http://localhost:3000/characters/${finder}`, {
+                method:"PATCH", 
+                headers:{
+                    "Content-Type": "application/json",
+                    "Accept": "application/json",
+                },
+                body: JSON.stringify({
+                    "votes": newVotes
+                })
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                let newVotes = data.votes
+            let updatedVotes = document.getElementById("vote-count")
+            updatedVotes.textContent = newVotes
+            })
         });
+        let resetBtn = document.getElementById("reset-btn")
+        resetBtn.addEventListener("click", (event)=> {
+            let finder = animal.id
+
+            fetch(`http://localhost:3000/characters/${finder}`, {
+                method:"PATCH", 
+                headers:{
+                    "Content-Type": "application/json",
+                    "Accept": "application/json",
+                },
+                body: JSON.stringify({
+                    "votes": 0
+                })
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            let updatedVotes = document.getElementById("vote-count")
+            updatedVotes.textContent = 0
+            })
+        })
     });
 
     function addVotes(animal){
@@ -39,7 +82,7 @@ function displayCharacters(animal) {
 }
 
 
-//add votes 
+
 
 
 getCharacter()
